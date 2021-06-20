3e6be8878dcdaeebcf2377a4eb401b811c6692f4
varsha213 <85276978+varsha213@users.noreply.github.com>
2021-06-20 12:09:09 +0530

    Add files via upload
    Basic banking system.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1" style="background-color : #black;">
+              
+            <div class="col-sm-12 col-md img text-center">
+                <img src="img/ba.jpg" class="img-fluid pt-2"><<label style="width=50px;"></label>
+              </div>
+              
+            </div>
+            
+            <h1 class="text-center pt-4"><u><label style="color : #131313;font-family: 'Playfair Display', serif;">WELCOME TO GRIP BANK</label></u></h1>
+            <h4 class="text-center pt-4"><label style="color : #131313;font-family: 'Playfair Display', serif;">Reach bank at your finger tip</label></h4>
+        
+      
+      <footer class="text-center mt-5 py-2">
+      <label style="color : #131313;font-family: 'Playfair Display', serif;"> <p><b>Done by VARSHA</b></p></label>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php" style="color : #fff;font-family: 'Playfair Display', serif;"> <b>GRIP BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #fff;font-family: 'Playfair Display', serif;"><b>HOME</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color :#fff;font-family: 'Playfair Display', serif;"><b>CREATE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color :#fff;font-family: 'Playfair Display', serif;"><b>REMOVE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #fff;font-family: 'Playfair Display', serif;"><b>TRANSFER MONEY</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color :#fff;font-family: 'Playfair Display', serif;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
